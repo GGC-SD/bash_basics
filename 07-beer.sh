7fb22f8520cf260ee71f354def8180bbc5205e08
David Hagerty <hagerty9009@gmail.com>
2018-10-21 10:53:28 -0400

    Add song about devils

--- 07-beer.sh
+++ 07-beer.sh
@@ -22,3 +22,17 @@ done
 
 # exercise: implement another counting song (such as 12 days of Christmas) 
 # using loops and if statements.
+
+echo "Let's sing a song about devils"
+echo "How many devils are there?"
+read devils
+
+while [ $devils -ge 0 ]; do
+  if [ $devils -ge 2 ]; then
+    echo "$devils little devils all dressed in red, tried to get to Heaven on the end of a thread\nThread-string got broken, down they all fell\nInstead of getting to Heaven, they went to..."
+  elif [ $devils -eq 1 ]; then
+    echo "$devils little devil all dressed in red, tried to get to Heaven on the end of a thread\nThread-string got broken, down he fell\nInstead of getting to Heaven, he went to BED!"
+  else
+    echo "No more devils"
+  fi
+done
