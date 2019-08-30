563cc67a6197379b0c92c5588dd1024d0f6321c9
Jonathan Mwizerwa <jmwizerw@ggc.edu>
2019-08-30 15:17:15 -0400

    Changed to also calculate number of pixels when provided with width and height

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,13 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+
+echo ""
+echo "Enter your width"
+read width
+echo "Enter your height"
+read height
+
+total=$((width*height))
+
+echo "The total number of pixels is $total"
