ee9d70a4cc6943e5cc920e133bd9d607a453f615
rcunico <rcunico@ggc.edu>
2019-09-07 12:35:19 -0400

    Modified 04-life.sh script to add time detection

--- 04-life.sh
+++ 04-life.sh
@@ -1,16 +1,23 @@
 
-echo "What is the meaning of life?"
-read meaning
+# echo "What is the meaning of life?"
+# read meaning
 
 
-if [ "$meaning" -eq 42 ]; then
-   echo "Yes!, That is the meaning of life!"
-else
-   echo "Awww... You don't know the meaning of life"
-fi
+# if [ "$meaning" -eq 42 ]; then
+#   echo "Yes!, That is the meaning of life!"
+# else
+#   echo "Awww... You don't know the meaning of life"
+# fi
 
 #  here are some other arithemetic comparison operators
 # -eq -ne -gt -ge -lt -le
 
 # exercise: write a script that prints whether it is 
 # morning or not
+
+  currenttime=$(date +%H:%M)
+   if [[ "$currenttime" < "12:00" ]] && [[ "$currenttime" > "00:01" ]]; then
+     echo "The current time is $currenttime AM, and it is morning!"
+   else
+     echo "The current time is $currenttime PM, and it is not morning."
+   fi
