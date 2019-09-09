e2a97d1cb72af1962952217dcbe1863cff7ca389
Nhorr <prwf97@gmail.com>
2019-09-08 22:44:28 -0400

    Did commented exercise in parallel universe

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,13 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+
+echo "------------------------"
+echo "Width:"
+read width
+echo "Height:"
+read height
+
+pixels=$(( width+height ))
+
+echo "pixels: $pixels"
