c8b40d2bc0ef84efd9bafffc4d8dad0b42112c7d
Chris Martinez <cmartinez5@ggc.edu>
2020-01-15 11:27:51 -0500

    my code is littleBuster.py and it is a song

---
+++ littleBuster.py
@@ -0,0 +1,69 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+#echo "Hello! What is your name"
+#read name
+#echo "Welcome, $name"
+
+# single quotes prevent the expansion of the variable
+#echo 'Your name was stored in $name'
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
+
+#echo "Hey $name, What is your filename?"
+#read filename
+#echo "You want $filename"
+#echo "Creating $filename ..."
+#touch $filename
+#echo "$filename creted"
+#ls
+#echo "Bye,bye"
+
+# So I Love this song but the chorus is about more than 
+# 50% ao the song is the chorus and is japanese which I 
+# know a thing or two about I hope you listen to it.
+#This is my first time using python and is not bad at all
+#LITTLE BUSTERS by The Pillows
+i = 0
+while i < 2:
+  print("With the kids sing out the future")
+  print("Maybe, kids don't need the masters")
+  print("Just waiting for the little Busters")
+  print("OH YEAHHHHHH")
+  print(" ")
+  i+=1
+
+print("YEAH YEAH YEAHHH!!!!")
+print("色あせないキッドナップミュージック")
+print("手と手が知っている")
+print("同じドアをノックしたい")
+print("この声が聞こえたら 飛び出して")
+print(" ")
+
+j = 0
+while j < 3:
+  print("With the kids sing out the future")
+  print("Maybe, kids don't need the masters")
+  print("Just waiting for the little Busters")
+  print("OH YEAHHHHHH")
+  print(" ")
+  j+=1
+
+print("YEAH YEAH YEAHHH!!!!")
+print("はずれやすいティーンエイジ・ギア")
+print("転がる日々も")
+print("空と海と大地はただ")
+print("あるがまま いつまでも逃げないぜ")
+print(" ")
+
+j = 0
+while j < 4:
+  print("With the kids sing out the future")
+  print("Maybe, kids don't need the masters")
+  print("Just waiting for the little Busters")
+  print("OH YEAHHHHHH")
+  print(" ")
+  j+=1
+
+print("YEAHHHH YEAHHH YEAHH!!!!....")
