bcf28764661fcbb4523e7496779ed5c6e6918070
ddavis60 <ddavis60@ggc.edu>
2020-01-15 12:07:08 -0500

    fixed script

--- 12-a_random_script.sh
+++ 12-a_random_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # read the name of the user and print hello
 
-count=0
+count=1
 
-while [ $count -lt 42 ]; do
+while [ $count -le 42 ]; do
     echo "So long and thanks for all the fish!"
     ((count=count+1))
 done
