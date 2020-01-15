14bc35def27ebd5f0e18ce63f2661f8d81366895
Anne <ajoseph13@ggc.edu>
2020-01-15 17:07:33 -0500

    An AI generated poem

---
+++ 01-read_input_ADM_copy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# An AI generated poem
+
+echo "Hi!, I am Anne"
+echo "This is an AI generated poem by POEMPORTRAITS"
+echo "The machine generates a sentence from a word we feed the machine"
+echo "Below is the poem created by AI by me feeding a bunch of words"
+echo "Hope you will enjoy this poem"
+echo "-----------------------------------------------------------------------------"
+echo "My dew and earth are laiid beneath the sunlight,"
+echo "Our hello has discovered the sea, the stars."
+echo "A streamlet with the sun and hair, and stars,"
+echo "Our faith."
+echo "This dream of the sea wears the wind."
+echo "Maggie and Zephyrus and the willows stand." 
+echo "Your dawn will be as wild as the sun is,"
+echo "Our delution to its object beat on the brightness."
+echo "-----------------------------------------------------------------------------"
+echo "The words I fed the AI are dew, streamlet, dream, and dawn"
+ 
+
