3005392175d4803e242842573bdd361fbb923190
Darom <dmercado@ggc.edu>
2020-01-30 14:53:07 -0500

    Ex3Completed2

--- 03-happy.sh
+++ 03-happy.sh
@@ -9,6 +9,15 @@ else
    echo "Still Smile :)"
 fi
 
+echo "Please enter what day is it"
+read answer
+
+if [ "$answer" = "Saturday" ] || [ "$answer" = "Sunday" ]; then
+   echo "Congratulations its the weekend!" 
+else
+   echo "Sorry It not the weekend yet"
+fi
+
 # here are the other string comparison operators
 # != , -n (not an empty string) , -z (an empty string)
 
