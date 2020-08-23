7f30dcee9a9b075dc19cd4fb07da29e9596f2f3f
Angel Mercado <amercadorodrigue@ggc.edu>
2020-08-22 22:24:07 -0400

    Assignment commit

---
+++ Angel_Mercado/script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Hello I'm Angel Mercado!"
+echo "I will have a blast coding this semester and learn a bunch from Dr. Anca! :) Team The Green Machine!"
+echo "Can you code? [y/n]"
+read input
+if [[ "$input" = "y" ]]; then
+  #statements
+  echo "That's great! keep learning"
+
+elif [[ "$input" = "n" ]]; then
+  #statements
+  echo "You should definitely try learning to code!"
+
+fi
+read enter
+echo "Goodbye!"
