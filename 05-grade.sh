53a2c7df194c2672517960261b248bac7b899478
Cesar Lopez <clopez9@ggc.edu>
2020-08-30 10:42:13 -0400

    new modification

--- 05-grade.sh
+++ 05-grade.sh
@@ -19,3 +19,18 @@ fi
 # that prints "it's cold" if the temperature is < 40
 # it's chilly if < 60, it's okay if < 70 and, it's hot for 
 # everything else
+
+echo "What is the temperature"
+echo "Please give a numeric answer"
+read temperature
+
+if [ $temperature -le 40 ]; then
+	echo "It's cold"
+elif [ $temperature -le 60 ]; then
+		echo "It's chilly"
+elif [ $temperature -le 70 ]; then
+		echo "It's okay"
+else 
+		echo "It's hot for everything else"
+fi
+		
