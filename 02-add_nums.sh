7f568a5a2994ebd7db1f953157956212e6cd936b
TrevLee5 <tlee7@ggc.edu>
2020-08-30 20:20:37 -0400

    attempting to create conflict

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,11 +13,11 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
-echo "Enter the width:"
+echo "Enter width:"
 read width
-echo "Enter the height"
+echo "Enter height"
 read height
 
-numberOfPixels=$(( width*height ))
+numPixels=$(( width*height ))
 
-echo "Number of pixels: $numberOfPixels"
+echo "Number of pixels: $numPixels"
