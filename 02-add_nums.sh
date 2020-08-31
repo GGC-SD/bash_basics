122c50dab0dabcb4976c526d56ce32c5d610a9a3
TrevLee5 <tlee7@ggc.edu>
2020-08-30 20:43:27 -0400

    Redid script

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -1,1 +1,24 @@
-Hello World
+#!/bin/sh
+
+echo "Enter your first number"
+read first
+echo "Enter your second number"
+read second
+echo "Enter your third number"
+read third
+
+sum=$(( first+second+third ))
+
+echo "The sum is $sum"
+
+# exercise: ask the user for the width and height and present total
+# number of pixels
+
+echo "Enter width"
+read width
+echo "Enter height"
+read height
+
+pixels=$(( width*height ))
+
+echo "Number of pixels: $pixels"
