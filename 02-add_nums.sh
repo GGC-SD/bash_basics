adf90778efe61f68576979bde60b19c66d3658d4
Dustin <dustcofer@gmail.com>
2020-08-30 20:28:19 -0400

    finding num of pixels rework

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,12 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+echo
+echo "Enter width of pixels"
+read width
+echo "Enter height of pixels"
+read height
+
+pixels=$(( width*height ))
+
+echo "There are $pixels pixels"
