7da3b4085834bd8b496e576d8be9735c36077a78
Matthew Rosario <mrosario1@ggc.edu>
2021-08-29 20:38:07 -0400

    Changed 04 file, added the exercise

--- 04-life.sh
+++ 04-life.sh
@@ -1,4 +1,12 @@
+time = `date +%H`
 
+if [ $time -lt 12 ]; then
+	echo "Good Morning!"
+else
+	echo "It is not morning"
+fi
+
+ 
 echo "What is the meaning of life?"
 read meaning
 
@@ -8,6 +16,9 @@ if [ "$meaning" -eq 42 ]; then
 else
    echo "Awww... You don't know the meaning of life"
 fi
+
+
+
 
 #  here are some other arithemetic comparison operators
 # -eq -ne -gt -ge -lt -le
