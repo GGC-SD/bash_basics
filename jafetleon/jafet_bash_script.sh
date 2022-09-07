0bd62c1ff17fd9d0596ddd58ba83633f133f3350
Jafet Leon-Perez <jafetleon12382@gmail.com>
2022-09-07 19:03:03 -0400

    adds personal bash script

---
+++ jafetleon/jafet_bash_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Jafet Manuel Leon-Perez"
+
+echo "This bash script was written in VSCode."
+
+echo "Count even numbers to 20:"
+
+n=20
+for ((i=2; i<=n; i+=2));
+do
+    echo $i
+done
