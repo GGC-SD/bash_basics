fa8235ba836565ca9b56b2fb5bee4bef57fe3f2f
BenMoreira <bmoreira@ggc.edu>
2022-09-08 10:11:03 -0400

    Adds new commands to happy file

--- 03-happy.sh
+++ 03-happy.sh
@@ -14,3 +14,12 @@ fi
 
 # exercise: write a script that prints whether today is
 # the weekend or not
+
+echo "What day is it?"
+read answer2
+
+if ["$answer2" = "friday"] || ["$answer2" = "saturday"] || ["$answer2" = "sunday"]; then
+	echo "It's the weekend! Let's go have some fun!"
+else
+	echo "It is not the weekend :(. I guess I have to go to work."
+fi
