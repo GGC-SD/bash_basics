bde8db531df195367205c9ca51f94b4e27cbb399
terrance-class <37383834+terrance-class@users.noreply.github.com>
2022-09-07 23:03:49 -0400

    Add files via upload

---
+++ jaredtext.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "my name is jared"
+echo "remebering c++"
+echo "did it work (yes or no)"
+read answer
+
+echo "$answer"
+if ($answer == no){
+echo "bad end"}
+else {
+echo "good end"}
+
+
