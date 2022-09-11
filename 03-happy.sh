a3b673b9d389c092249ea7e34055a7a97ce93806
Joseph Kolarik <jkolarik@ggc.edu>
2022-09-11 18:13:22 -0400

    Undoes modifications made by Joseph Kolarik

--- 03-happy.sh
+++ 03-happy.sh
@@ -3,8 +3,7 @@
 echo "You are happy?"
 read answer
 
-# modified to be case insensitive by converting answer to lowercase and then comparing
-if [ "${answer,,}" = "yes" ] || [ "${answer,,}" = "ye" ] || [ "${answer,,}" = "y" ]; then
+if [ "$answer" = "yes" ]; then
    echo "Smile :)"
 else
    echo "Still Smile :)"
@@ -15,19 +14,3 @@ fi
 
 # exercise: write a script that prints whether today is
 # the weekend or not
-
-# day code taken from 10-greeter.sh
-day=$(date | cut -d' ' -f1)  # you can use this form instead of backticks
-
-if [ $day = "Sat" ] || [ $day = "Sun" ]; then
-   echo "I hope you will enjoy your weekend."
-   echo "And if you have work on the weekend,"
-   echo "you'll get through it"
-else
-   echo "You can get through the week."
-   echo "Nothing's stopped you yet"
-fi
-
-echo ""
-echo "ENTER anything to close"
-read tmp
