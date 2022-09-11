63e99349fa9f961807528a5b5dd7d8027ccd2e9e
Joseph Kolarik <jkolarik@ggc.edu>
2022-09-11 16:49:02 -0400

    Fixes JK_Script.sh to fit assignment requirements.

--- Joseph_Kolarik/JK_Script.sh
+++ Joseph_Kolarik/JK_Script.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
-# read the name of the user and print hello
 
-echo "Hello! What is your name"
+echo "My name is Joseph Kolarik"
+echo "Whats your name?"
 read name
-echo "Welcome, $name"
+echo "I hope you have a wonderful day, $name"
 
-# single quotes prevent the expansion of the variable
-echo 'Your name was stored in $name'
-
-# exercise: write a script that asks the user for a 
-# filename and create an empty file named after it
-
-echo "Hey $name, How many times do you want me to say 'Hi'?"
+echo "How many times do you want me to say 'Hello'?"
 read count
 echo "You want to loop $count times!"
 
 while [ $count -gt 0 ]
 do
-    echo "Hi!"
+    echo "Hello, $name!"
     let count-=1
 done
 echo "ENTER anything to close"
