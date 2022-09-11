dc67faf73606292a7b59067797ea5ff6e3712a78
Joseph Kolarik <jkolarik@ggc.edu>
2022-09-11 17:36:42 -0400

    Modifies 03-happy.sh to fullfill internal exercise.

--- 03-happy.sh
+++ 03-happy.sh
@@ -3,7 +3,8 @@
 echo "You are happy?"
 read answer
 
-if [ "$answer" = "yes" ]; then
+# modified to be case insensitive by converting answer to lowercase and then comparing
+if [ "${answer,,}" = "yes" ] || [ "${answer,,}" = "ye" ] || [ "${answer,,}" = "y" ]; then
    echo "Smile :)"
 else
    echo "Still Smile :)"
@@ -14,3 +15,17 @@ fi
 
 # exercise: write a script that prints whether today is
 # the weekend or not
+
+# day code taken from 10-greeter.sh
+day=$(date | cut -d' ' -f1)  # you can use this form instead of backticks
+
+if [ $day = "Sat" ] || [ $day = "Sun" ]; 
+then
+   echo "I hope you will enjoy your weekend."
+else
+   echo "You'll get through the week."
+fi
+
+echo ""
+echo "ENTER anything to close"
+read tmp
