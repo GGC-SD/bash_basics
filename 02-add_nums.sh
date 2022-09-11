c0eac52bf23c9db4be4166bd43269862d0b73a26
Jeancarlo Masciotti <jkiaba@protonmail.com>
2022-09-11 11:01:58 -0400

    New commands added to bash script 02

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -33,3 +33,16 @@ read height
 echo ""
 pixelTotal=$(( $width * $height ))
 echo "Your display has ${pixelTotal} pixels." 
+
+# find the product of 4 numbers from user input
+echo "Find the product of 4 numbers that you will enter..."
+echo "Enter the first number..."
+read num1
+echo "Enter the second number..."
+read num2
+echo "Enter the third number..."
+read num3
+echo "Enter the fourth number..."
+read num4
+product=$(( $num1 * $num2 * $num3 * $num4 ))
+echo "The product is ${product}..."
