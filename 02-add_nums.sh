b1c860f71a5a0f0972b557bc7dd35976d20183a2
Harmones81 <52055382+Harmones81@users.noreply.github.com>
2023-08-26 14:53:16 -0400

    edited the '02-add_nums.sh' file

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -33,3 +33,20 @@ read height
 echo ""
 pixelTotal=$(( $width * $height ))
 echo "Your display has ${pixelTotal} pixels." 
+
+# finding the slope between two points
+
+echo ""
+echo "Enter the x-coordinate for the first point."
+read x1
+echo "Enter the y-coordinate for the first point."
+read y1
+echo "Enter the x-coordinate for the second point."
+read x2
+echo "Enter the y-coordinate for the second point."
+read y2
+let numerator=$y2-$y1
+let denominator=$x2-$x1
+let slope=$numerator/$denominator
+
+echo "The slope between the two points is $slope."
