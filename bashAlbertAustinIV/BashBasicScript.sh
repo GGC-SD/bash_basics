e340f9c6f89380301c0be669367ee75348f81b39
Albert Austin IV <albert.austiniv@gmail.com>
2023-08-27 23:20:49 -0400

    Created script - Albert Austin IV

---
+++ bashAlbertAustinIV/BashBasicScript.sh
@@ -0,0 +1,11 @@
+echo "Albert Austin IV"
+echo "Nice to see you!"
+
+echo "Are you having a good day? (yes/no)"
+read answer
+
+if [ "$answer" = "yes" ]; then
+	echo "I'm glad!"
+else
+	echo "I hope your day gets better!"
+fi
