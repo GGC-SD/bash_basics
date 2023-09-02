06ccc0b9c928a617f39f2fe24f2f0b2472938ab1
hmbarnez <hmbarnes007@gmail.com>
2023-09-01 20:00:38 -0400

    added something dif to script 10-greeter

--- 10-greeter.sh
+++ 10-greeter.sh
@@ -4,24 +4,24 @@
 
 function weekend_greet 
 {
-   echo "Enjoy the weekend! ${1}!"
+   echo "Hope you are working hard, ${1}! It's ${2} today, unfortunately."
 }
 
 function weekday_greet
 {
-   echo "Hope you are working hard, ${1}!"
+   echo "Hope you are working hard, ${1}! It's ${2} today."
 }
-
 
 echo "Please enter your name"
 read name
 
-day=$(date | cut -d' ' -f1)  # you can use this form instead of backticks
+day=$(date | cut -d' ' -f1)
+current_day=$(date '+%A')  # Get the full day name
 
 if [ $day = "Sat" ] || [ $day = "Sun" ]; then
-   weekend_greet $name
+   weekend_greet $name $current_day
 else
-   weekday_greet $name
+   weekday_greet $name $current_day
 fi
 
 # exercise: Add a second argument to the function 
