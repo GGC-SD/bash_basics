5b849e198c0d74621632a6c7e977baa3efe4d905
Alexis Pardo <alexispardo27@gmail.com>
2023-09-03 22:09:47 -0400

    Writes a script that checks the weather.

--- 05-grade.sh
+++ 05-grade.sh
@@ -15,7 +15,19 @@ else
    echo "Time to work on some extra credit assignments"
 fi
 
-# exercise: write a script (that utilizes weather-util) 
+# exercise: write a script (that utilizes weather-util)
 # that prints "it's cold" if the temperature is < 40
-# it's chilly if < 60, it's okay if < 70 and, it's hot for 
+# it's chilly if < 60, it's okay if < 70 and, it's hot for
 # everything else
+
+temperature=$(weather -t)
+
+if [ "$temperature" -lt 40 ]; then
+  echo "It's cold out."
+elif [ "$temperature" -lt 60 ]; then
+  echo "It's chilly today."
+elif [ "$temperature" -lt 70 ]; then
+  echo "It's okay."
+else
+  echo "It's hot today."
+fi
