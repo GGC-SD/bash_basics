68242b039fc3889c38cec3ab05cc0b74bd6a384a
rsingh021 <ravjotsingh1434@gmail.com>
2023-09-03 23:03:54 -0400

    Update 01-read_input.sh

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -4,7 +4,7 @@
 echo "Hello! What is your name"
 read name
 echo "Welcome, $name"
-
+#This is ravjot's comment editing the file that is changed
 # single quotes prevent the expansion of the variable
 echo 'Your name was stored in $name'
 
