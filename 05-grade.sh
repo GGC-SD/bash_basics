eee244b592ffa25f6efd13354c3d5e9383faf0ac
Danielle <mathieudanielle0@gmail.com>
2024-01-26 20:52:45 -0500

    added log graph picture

--- 05-grade.sh
+++ 05-grade.sh
@@ -19,3 +19,15 @@ fi
 # that prints "it's cold" if the temperature is < 40
 # it's chilly if < 60, it's okay if < 70 and, it's hot for 
 # everything else
+echo "What is the temperature outside right now?"
+read temp
+
+if [ $temp -le 40 ]; then
+   echo "It's cold."
+elif [ $temp -le 60 ]; then
+   echo "It's chilly"
+elif [ $temp -le 70 ]; then
+   echo "It's okay"
+else
+   echo "It's hot"
+fi
