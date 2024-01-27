a087ee4445e41f8972ccf7c9aa08e4b99a53dc54
Danielle <mathieudanielle0@gmail.com>
2024-01-26 20:24:16 -0500

    Did 05 git exercise

--- 05-grade.sh
+++ 05-grade.sh
@@ -19,3 +19,13 @@ fi
 # that prints "it's cold" if the temperature is < 40
 # it's chilly if < 60, it's okay if < 70 and, it's hot for 
 # everything else
+echo "How many degrees is it outside today?"
+read temp
+
+if [ $temp -ge 70 ]; then
+   echo "It's hot"
+elif [ $temp -ge 60 ]; then
+   echo "It's okay."
+else
+   echo "It's cold!"
+fi
