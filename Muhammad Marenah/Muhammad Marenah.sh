9678217eef3c39813dd28e69b1046b48bbdc80e7
Muhammad Marenah <51488699+marenah@users.noreply.github.com>
2024-01-28 23:27:11 -0500

    Added Folder and File Muhammad Marenah
    File "Muhammad Marenah" added with the code echo "Muhammad Marenah" and a folder . A nice message and a while loop

--- Muhammad Marenah/Muhammad Marenah.sh
+++ Muhammad Marenah/Muhammad Marenah.sh
@@ -1,1 +1,13 @@
-echo "Muhammad Marenah"
+# Print my name
+echo "Muhammad"
+
+# Print a nice message
+echo "Good Day"
+
+# Use a while loop to count
+counter=1
+while [ $counter -le 5 ]
+do
+    echo "Count: $counter"
+    ((counter++))
+done
