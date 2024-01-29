959760767d35080d80debcb0f211a4db0b4b8e9f
KietLy <kito_ken123@outlook.com>
2024-01-28 21:17:23 -0500

    File modified so it meets the requirement on Piazza.

--- Kiet/01-read_input_ADM - Copy.sh
+++ Kiet/01-read_input_ADM - Copy.sh
@@ -2,3 +2,10 @@
 # read the name of the user and print hello
 
 echo "Welcome! My name is Kiet."
+
+echo "Welcome to my script!"
+
+echo "Counting from 1 to 5:"
+for i in {1..5}; do
+    echo "$i"
+done
