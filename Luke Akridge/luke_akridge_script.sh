3841d988565439ea945e1ec1242a6c4edebd7c0b
Luke Akridge <lakridge@ggc.edu>
2024-01-28 21:09:09 -0500

    Changes for the github homework assignment

---
+++ Luke Akridge/luke_akridge_script.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+echo "My name is Luke Akridge."
+echo "This is my homework project."
+while (true)
+	echo "This loop should terminate here."
+	done
+echo "The loop should be terminated."
