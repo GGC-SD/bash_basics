a38929041d04d445e68a72a906621005f9616852
KietLy <kito_ken123@outlook.com>
2024-02-04 20:21:36 -0500

    Update 03-happy.sh
    Ver2 of 03-happy.sh

--- 03-happy.sh
+++ 03-happy.sh
@@ -14,3 +14,8 @@ fi
 
 # exercise: write a script that prints whether today is
 # the weekend or not
+if [ $(date +%u) -gt 5 ]; then
+    echo "Today is the weekend. =)"
+else
+    echo "Today is not the weekend. =("
+fi
