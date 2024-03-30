79bbb7f8f9ead2cfd202b5bb8c90daf617a76c95
James <jshaklee2@ggc.edu>
2024-03-29 20:35:18 -0400

    changed 01-read_input.sh

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -10,3 +10,10 @@ echo 'Your name was stored in $name'
 
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
+
+#Alex's changes
+echo "Enter a name for the new file"
+read newFile
+touch $newFile.txt
+echo "$newFile.txt has been created!"
+echo 'The file name was stored in $newFile'
