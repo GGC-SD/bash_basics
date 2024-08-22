54b21ab55da76e015a2f89606fe6d8e1782e3f31
abruce4 <abruce4@ggc.edu>
2024-08-21 22:26:48 -0400

    Lincoln Bruce adding folder and script

---
+++ LincolnBruce/01-abruce4.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# exercise: print your name
+echo "Lincoln Bruce"
+
+# exercise: print a message
+echo "Hi, I hope you are fine"
+
+# exercise: print something using loop
+for i in {1..5}; do 
+echo "Hello World $i"
+done
+
+read
+
