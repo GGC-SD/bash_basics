7b295cf2a75a5a81d1e677f046cd0ddce8ed4430
Aniela <anielaelorza@gmail.com>
2024-08-28 22:54:12 -0400

    Add basic bash script

---
+++ Aniela/01-bash_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# print my name from the command line
+echo "Aniela Elorza"
+
+# print a nice message 
+echo "Hi! I hope you have a great day today!"
+
+# use either while, do-while, for, or if statement to do some task 
+declare -i a=9
+
+if (($a>8))
+then
+    echo "$a is greater than 8!"
+else 
+    echo "$a is not greater than 8!"
+fi 
