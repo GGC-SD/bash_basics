cbaa749051c2e8309a6301001063d474bd74bc55
Aniela <anielaelorza@gmail.com>
2024-09-01 13:15:15 -0400

    Add 'Aniela' folder and basic bash script

---
+++ Aniela/01-bash_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# print name from the command line
+
+echo "Aniela Elorza"
+
+# print nice message
+
+echo "I hope you have a great day today!"
+
+# exercise: use either while, do-while, for, or if statement to do some task of your choice
+
+declare -i a=9
+
+if (($a>8))
+then 
+    echo "$a is greater than 8!"
+else 
+    echo "$a is not greater than 8!"
+fi
+
+
+
