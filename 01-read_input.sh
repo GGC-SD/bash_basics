4d2063891f6fb4ea8e08cf300ababf822c6f6809
J Haines <jhaines2@ggc.edu>
2024-09-01 20:46:16 -0400

    Edited read input file.

---
+++ 01-read_input.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+#Edited again, lines 5, 6, 7, 8 are changed.
+echo "Enter name for new file that will be created"
+read file_name
+touch $file_name
+echo "File successfully created"
+
+# read name
+# echo "Welcome, $name"
+
+# single quotes prevent the expansion of the variable
+# echo 'Your name was stored in $name'
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
