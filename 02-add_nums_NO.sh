5ea1f69a67e65f0ad6cfda57a5ca0e96293fe729
Nicholas Opuni <nickopuni2429@gmail.com>
2024-09-01 21:38:22 -0400

    Edited and remaned file to 02-add_nums_NO.sh

--- 02-add_nums_NO.sh
+++ 02-add_nums_NO.sh
@@ -8,8 +8,8 @@ echo "Enter your third number"
 read third
 
 sum=$(( first+second+third ))
-
 echo "The sum is $sum"
+
 
 echo "Please enter the height"
 read height
@@ -17,8 +17,8 @@ echo "Please enter the width"
 read width
 
 totalPixels=$(( height*width ))
-
 echo "The total number of pixels is $totalPixels"
+
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
@@ -33,3 +33,32 @@ read height
 echo ""
 pixelTotal=$(( $width * $height ))
 echo "Your display has ${pixelTotal} pixels." 
+
+
+# exercise: ask the user for positive or negative numbers
+
+echo ""
+echo "Please enter a number: "
+read num
+echo ""
+if [ $num -gt 0 ]; then
+  echo "$num is positive"
+elif [ $num -lt 0 ]; then
+  echo "$num is negative"
+else
+  echo "$num is zero"
+fi
+
+
+# exercise: ask the user for even or odd numbers
+
+echo ""
+echo "Please enter a number: "
+read num
+echo ""
+even=$( expr $num % 2 )
+if [ $even -eq 0 ]; then
+  echo "$num is even"
+else
+  echo "$num is odd"
+fi
