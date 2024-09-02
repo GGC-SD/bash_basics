ae60af7fd4931ecfd7405966d0e1828748bf59a5
Destiny <dlowe7@ggc.edu>
2024-09-02 09:35:19 -0400

    11-song edited by Destiny Lowe with new functionality

--- 11-song.sh
+++ 11-song.sh
@@ -4,41 +4,41 @@
 while [ true ]; do
 
    # you can show a prompt with the read command
-   read -p "Do rae mi fa so la ti do? (q to end) > " note
+   read -p "What is your favorite day of the week? (q to end) > " day
 
-case $note in
+case $day in
    # each case matches a pattern
-   do|Do)
-      echo "Doe a deer a female deer"
-      ;;   
-   rae|Rae)
-      echo "Ray a drop of golden sun"
+   Monday|monday)
+      echo "Monday: Start of the week. Unfortunately, its time to get to work!"
       ;;
-   mi|Mi)
-      echo "Me a name a call myself"
+   Tuesday|tuesday)
+      echo "Tuesday: Still tired from Monday? It's okay, we're in the groove."
       ;;
-   fa|Fa)
-      echo "Far a long long way to run"
+   Wednesday|wednesday)
+      echo "Wednesday: Happy Hump Day! It's the middle of the week!"
       ;;
-   so|So)
-      echo "So a note that follows fa"
+   Thursday|thursday)
+      echo "Thursday: One more day until Friday!"
       ;;
-   la|La)
-      echo "La a note that follow so"
+   Friday|friday)
+      echo "Friday: It's Friday! Its the end of the week and the last day!"
       ;;
-   ti|ta)
-      echo "Tea I drink with jam and bread"
+   Saturday|saturday)
+      echo "Saturday: Time for fun and errands, but more importantly fun!"
+      ;;
+   Sunday|sunday)
+      echo "Sunday: Unfortunately, Monday is approaching again, lets rest today."
       ;;
    q)
-      echo "Hope you enjoyed the sound of music"
+      echo "Goodbye! Enjoy your week!"
       exit 0
       ;;
-   *) 
-      echo "Not a note"
+   *)
+      echo "That's not a day of the week!"
       ;;
 esac
 
 done
 
-# exercise: Change the input question and case 
-# statement to another theme. 
+# exercise: Change the input question and case
+# statement to another theme.
