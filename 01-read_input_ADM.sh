e3140e7cfb3d1cffe91e23598967671e5b2ea7e2
jan <jkaltenegger@ggc.edu>
2025-01-29 17:49:25 -0500

    Edited 01-read_input_ADM

--- 01-read_input_ADM.sh
+++ 01-read_input_ADM.sh
@@ -8,14 +8,14 @@ echo "Welcome, $name"
 # single quotes prevent the expansion of the variable
 echo 'Your name was stored in $name'
 
-# exercise: write a script that asks the user for a 
+# exercise: write a script that asks the user for a
 # filename and create an empty file named after it
 
-echo "Hey $name, What is your filename?"
+echo "$name, what do you want your file to be named as?"
 read filename
-echo "You want $filename"
+echo "You want $filename?"
 echo "Creating $filename ..."
 touch $filename
-echo "$filename creted"
+echo "$filename created"
 ls
-echo "Bye,bye"
+echo "Cya"
