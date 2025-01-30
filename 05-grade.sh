9f834e13ed1b8338ac785aa2af8cb8b7efa0669b
tonismacbook <jdada@ggc.edu>
2025-01-30 14:27:48 -0500

    Bash assignment 5b / 05-grade.sh

--- 05-grade.sh
+++ 05-grade.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 
-echo "What did you get in the first ITEC 3860 test?"
-echo "Please give a numeric answer"
-read grade
+echo "Hello, lets find out how hot or cold your weather is!"
+echo "What is your temperature?"
+# shellcheck disable=SC2162
+read temperature
 
-if [ $grade -ge 90 ]; then
-   echo "You got an A. Nice."
-elif [ $grade -ge 80 ]; then
-   echo "You got a B. That's good."
-elif [ $grade -ge 70 ]; then
-   echo "You got a C. Not bad."
+if [ "$temperature" -lt 40 ]; then
+  echo "Its cold"
+elif [ "$temperature" -ge 40 ] && [ "$temperature" -lt 60 ]; then
+  echo "It's chilly"
+
+elif [ "$temperature" -ge 60 ] && [ "$temperature" -lt 70 ]; then
+  echo "It's okay"
+
 else
-   echo "Time to work on some extra credit assignments"
+  echo "It's hot"
+
 fi
 
-# exercise: write a script (that utilizes weather-util) 
-# that prints "it's cold" if the temperature is < 40
-# it's chilly if < 60, it's okay if < 70 and, it's hot for 
-# everything else
