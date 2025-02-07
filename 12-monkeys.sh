ec4a51e78e3c3020f9b78da1a457200cc724adc4
Oved Lomeli <olomeli@ggc.edu>
2025-02-07 13:23:36 -0500

    Assignment 5b. Completed the exercise in 12-monkeys.sh and pushing to a new branch.

--- 12-monkeys.sh
+++ 12-monkeys.sh
@@ -1,18 +1,18 @@
 # exercise: implement another counting song (such as 12 days of Christmas)
 # using loops and if statements.
 
-echo "Now, let's sing monkey jumping on the bed."
-echo "How many monkeys are jumping on the bed?"
+echo "Now, let's count sheep to go to sleep!"
+echo "How many sheep are there?"
 read number
+
+echo "Count the sheep!"
+echo "Go to sleep!"
+echo "Hope you have, a wonderful dream!"
 while [ $number -ge 0 ]; do
-  if [ $number -ge 2 ]; then
-    echo "$number little monkeys jumping on the bed. One fell off and bumped its head."
-    echo "Momma called the doctor and the doctor said, 'No more Monkeys jumping on the bed!'"
-  elif [ $number -ge 1 ]; then
-    echo "$number little monkeys jumping on the bed. One fell off and bumped its head."
-    echo "Momma called the doctor and the doctor said, 'No more Monkeys jumping on the bed!'"
+  if [ $number -ge 1 ]; then
+    echo "$number sheep!"
   else
-    echo "No more monkeys jumping on the bed."
+    echo "Goodnight! ZZZZZzzzzzzzz"
   fi
 
   ((number = number - 1))
